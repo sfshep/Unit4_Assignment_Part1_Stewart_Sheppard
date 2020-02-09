68828a6038495893e19a1c62900c63984e5a4e60
Fred Sheppard <stewart.sheppard@my.tccd.edu>
2020-02-09 04:23:04 +0000

    Basic Setup

--- main.cpp
+++ main.cpp
@@ -1,5 +1,19 @@
-#include <iostream>
+/*
+Stewart Sheppard
+Unit4_Assignment- Loops (Repetition Structures)
+COSC 1436 570002
+Repl.it / Github
+This program will work with counters and perform calculations.
+*/
+#include <cstdlib>  // Provides EXIT_SUCCESS
+#include <iostream>  // Provides cout, cerr, endl
+#include "Input_Validation_Extended.h" // Provides getValidatedInput
+#include<string> //for string manipulation
+#include<sstream> //for string manipulation and comparison
 
-int main() {
-  std::cout << "Hello World!\n";
+using namespace std;
+
+int main() 
+{
+  
 }
